224be2e29565b114d20c1df30aa0b90e697a9cbb
David Adrian <davidcadrian@gmail.com>
2015-05-17 18:10:46 -0400

    Seems to be working?

---
+++ check/checks/export.go
@@ -0,0 +1,59 @@
+package checks
+
+import (
+	"errors"
+	"log"
+	"net"
+
+	"github.com/zmap/zgrab/ztools/ztls"
+)
+
+func CheckExport(c net.Conn, name string) (*ztls.DHParams, error) {
+	config := new(ztls.Config)
+	config.CipherSuites = ztls.DHEExportCiphers
+	config.ServerName = name
+	config.InsecureSkipVerify = true
+	config.MinVersion = ztls.VersionSSL30
+	config.ForceSuites = true
+	tlsConn := ztls.Client(c, config)
+	tlsConn.Handshake()
+	hl := tlsConn.GetHandshakeLog()
+	if hl == nil {
+		return nil, nil
+	}
+	return hl.DHExportParams, nil
+}
+
+func CheckDHE(c net.Conn, name string) (*ztls.DHParams, error) {
+	config := new(ztls.Config)
+	config.ServerName = name
+	config.InsecureSkipVerify = true
+	config.CipherSuites = ztls.DHECiphers
+	config.MinVersion = ztls.VersionSSL30
+	config.ForceSuites = true
+	tlsConn := ztls.Client(c, config)
+	he := tlsConn.Handshake()
+	log.Print(he)
+	hl := tlsConn.GetHandshakeLog()
+	log.Print(hl)
+	if hl == nil {
+		return nil, nil
+	}
+	return hl.DHParams, nil
+}
+
+func CheckChrome(c net.Conn, name string) (*ztls.DHParams, error) {
+	config := new(ztls.Config)
+	config.ServerName = name
+	config.CipherSuites = ztls.ChromeCiphers
+	config.InsecureSkipVerify = true
+	config.MinVersion = ztls.VersionSSL30
+	config.ForceSuites = true
+	tlsConn := ztls.Client(c, config)
+	tlsConn.Handshake()
+	hl := tlsConn.GetHandshakeLog()
+	if hl == nil || hl.ServerCertificates == nil {
+		return nil, errors.New("does not support TLS")
+	}
+	return hl.DHParams, nil
+}
--- check/handlers/handlers.go
+++ check/handlers/handlers.go
@@ -1,25 +1,37 @@
 package handlers
 
 import (
-	"errors"
 	"log"
 	"net"
+	"net/http"
+	"sync"
+	"time"
 
+	"github.com/dadrian/weakdh/check/checks"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/zmap/zgrab/ztools/ztls"
 )
 
 type ServerCheck struct {
-	Domain         string
-	IP             net.IP
-	DHParams       *ztls.DHParams
-	ExportDHParams *ztls.DHParams
+	Domain  string             `json:"domain"`
+	IP      []net.IP           `json:"ip_addresses"`
+	Results []*SingleHostCheck `json:"results"`
+}
+
+type SingleHostCheck struct {
+	Domain         string         `json:"-"`
+	IP             string         `json:"ip"`
+	HasTLS         bool           `json:"has_tls"`
+	DHParams       *ztls.DHParams `json:"dh_params"`
+	ExportDHParams *ztls.DHParams `json:"export_dh_params"`
+	ChromeDHParams *ztls.DHParams `json:"chrome_dh_params"`
+	Error          error          `json:"error"`
 }
 
 type ServerCheckParams struct {
 	Server   string `form:"server" binding:"required"`
-	Protocol string ``
+	Protocol string
 }
 
 type checkHandler struct {
@@ -44,7 +56,9 @@ func bindParams() gin.HandlerFunc {
 			c.AbortWithStatus(400)
 		}
 		// Check if IP or domain
-		if check.IP = net.ParseIP(p.Server); check.IP == nil {
+		if ip := net.ParseIP(p.Server); ip != nil {
+			check.IP = []net.IP{ip}
+		} else {
 			check.Domain = p.Server
 		}
 		c.Next()
@@ -55,43 +69,112 @@ func dnsLookup() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		log.Print("DNS Lookup")
 		check := c.MustGet("check").(*ServerCheck)
-		if check.IP != nil {
-			log.Print("Have an IP, no need to DNS lookup")
+		if check.IP == nil {
+			// We have a domain, lookup IP addresses
+			addrs, err := net.LookupIP(check.Domain)
+			if err != nil {
+				c.Error(err, "DNS Lookup Failed")
+				c.AbortWithStatus(500)
+			}
+			hostCount := len(addrs)
+			check.IP = addrs
+			check.Results = make([]*SingleHostCheck, hostCount)
 		} else {
-			c.Error(errors.New("Only IPs supported"), nil)
-			c.AbortWithStatus(400)
+			check.Results = make([]*SingleHostCheck, 1)
 		}
 		c.Next()
 	}
 }
 
-func startHandshakes() gin.HandlerFunc {
+func handshakes() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		log.Print("start handshakes")
 		check := c.MustGet("check").(*ServerCheck)
-		config := new(ztls.Config)
-		config.InsecureSkipVerify = true
-		address := net.JoinHostPort(check.IP.String(), "443")
-		conn, err := net.Dial("tcp", address)
-		if err != nil {
-			c.Error(err, nil)
-			c.AbortWithStatus(500)
+		domain := check.Domain
+		log.Print(domain)
+		allHostsGroup := new(sync.WaitGroup)
+		allHostsGroup.Add(len(check.IP))
+		for idx, addr := range check.IP {
+			go func(i int, a net.IP, wg *sync.WaitGroup) {
+				defer wg.Done()
+				checkGroup := new(sync.WaitGroup)
+				checkGroup.Add(3)
+				fullAddress := net.JoinHostPort(a.String(), "443")
+				var normal, export, chrome *ztls.DHParams
+				var noTLS bool
+				var connErr error
+
+				setErrorOnce := new(sync.Once)
+
+				dialer := new(net.Dialer)
+				dl := time.Now().Add(time.Second * 10)
+				dialer.Deadline = dl
+
+				go func(fullAddress string, cg *sync.WaitGroup) {
+					defer cg.Done()
+					conn, err := dialer.Dial("tcp", fullAddress)
+					if err != nil {
+						setErrorOnce.Do(func() {
+							connErr = err
+						})
+						return
+					}
+					conn.SetDeadline(dl)
+					export, _ = checks.CheckExport(conn, domain)
+				}(fullAddress, checkGroup)
+
+				go func(fullAddress string, cg *sync.WaitGroup) {
+					defer cg.Done()
+					conn, err := dialer.Dial("tcp", fullAddress)
+					if err != nil {
+						setErrorOnce.Do(func() {
+							connErr = err
+						})
+						return
+					}
+					conn.SetDeadline(dl)
+					normal, _ = checks.CheckDHE(conn, domain)
+				}(fullAddress, checkGroup)
+
+				go func(fullAddress string, cg *sync.WaitGroup) {
+					defer cg.Done()
+					conn, err := dialer.Dial("tcp", fullAddress)
+					if err != nil {
+						setErrorOnce.Do(func() {
+							connErr = err
+						})
+						return
+					}
+					conn.SetDeadline(dl)
+					chrome, err = checks.CheckChrome(conn, domain)
+					if err != nil {
+						noTLS = true
+					}
+				}(fullAddress, checkGroup)
+
+				checkGroup.Wait()
+				out := &SingleHostCheck{
+					HasTLS:         !noTLS,
+					DHParams:       normal,
+					ExportDHParams: export,
+					ChromeDHParams: chrome,
+					Error:          connErr,
+				}
+				log.Print(i)
+				check.Results[i] = out
+
+			}(idx, addr, allHostsGroup)
 		}
-		tlsConn := ztls.Client(conn, config)
-		if err = tlsConn.Handshake(); err != nil {
-			c.Error(err, nil)
-			c.AbortWithStatus(500)
-		}
-		h := tlsConn.GetHandshakeLog()
-		c.JSON(200, h)
+		allHostsGroup.Wait()
+		c.Next()
 	}
 }
 
 func checkServer(c *gin.Context) {
-	//	c.JSON(http.StatusOK, gin.H{"server": "yolo"})
+	check := c.MustGet("check").(*ServerCheck)
+	c.JSON(http.StatusOK, check)
 }
 
 func UseServerCheck(g *gin.RouterGroup) {
-	g.Use(addServerCheck(), bindParams(), dnsLookup(), startHandshakes())
+	g.Use(addServerCheck(), bindParams(), dnsLookup(), handshakes())
 	g.GET("/", checkServer)
 }
