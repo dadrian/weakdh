213dcb5c8dd62433365a8a007954cbf58a69fa7f
David Adrian <davidcadrian@gmail.com>
2015-05-17 15:16:01 -0400

    Connect to a TLS server on the backside
    Superhacky, still needs work.

--- check/handlers/handlers.go
+++ check/handlers/handlers.go
@@ -1,9 +1,9 @@
 package handlers
 
 import (
+	"errors"
 	"log"
 	"net"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
@@ -41,6 +41,7 @@ func bindParams() gin.HandlerFunc {
 		p := new(ServerCheckParams)
 		if c.BindWith(p, binding.Form) != true {
 			c.Error(errBadCheckParams, nil)
+			c.AbortWithStatus(400)
 		}
 		// Check if IP or domain
 		if check.IP = net.ParseIP(p.Server); check.IP == nil {
@@ -53,15 +54,44 @@ func bindParams() gin.HandlerFunc {
 func dnsLookup() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		log.Print("DNS Lookup")
+		check := c.MustGet("check").(*ServerCheck)
+		if check.IP != nil {
+			log.Print("Have an IP, no need to DNS lookup")
+		} else {
+			c.Error(errors.New("Only IPs supported"), nil)
+			c.AbortWithStatus(400)
+		}
 		c.Next()
 	}
 }
 
+func startHandshakes() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		log.Print("start handshakes")
+		check := c.MustGet("check").(*ServerCheck)
+		config := new(ztls.Config)
+		config.InsecureSkipVerify = true
+		address := net.JoinHostPort(check.IP.String(), "443")
+		conn, err := net.Dial("tcp", address)
+		if err != nil {
+			c.Error(err, nil)
+			c.AbortWithStatus(500)
+		}
+		tlsConn := ztls.Client(conn, config)
+		if err = tlsConn.Handshake(); err != nil {
+			c.Error(err, nil)
+			c.AbortWithStatus(500)
+		}
+		h := tlsConn.GetHandshakeLog()
+		c.JSON(200, h)
+	}
+}
+
 func checkServer(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"server": "yolo"})
+	//	c.JSON(http.StatusOK, gin.H{"server": "yolo"})
 }
 
 func UseServerCheck(g *gin.RouterGroup) {
-	g.Use(addServerCheck(), bindParams(), dnsLookup())
+	g.Use(addServerCheck(), bindParams(), dnsLookup(), startHandshakes())
 	g.GET("/", checkServer)
 }
